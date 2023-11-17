571749292c390c6f418da37d0c803256a8057861
tc94 <tze.chong1994@gmail.com>
2023-11-17 09:31:32 +0800

    tic-tac-toe with minimax algo

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,4 +1,4 @@
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 import time
 
 class TicTacToe:
@@ -120,6 +120,6 @@ def play(game, x_player, o_player, print_game=True):
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
+    o_player = GeniusComputerPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -55,3 +55,40 @@ class GeniusComputerPlayer(Player):
     def minimax(self, state, player):
         max_player = self.letter 
         other_player = 'O' if player == 'X' else 'X'
+
+        # first, we want to check if the previous move is a winner
+        # this is our base case
+        if state.current_winner == other_player:
+            #we should return position and score because we need to keep track of the score
+            # for minimax to work
+            return {'position': None,
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1
+                    )}
+        elif not state.empty_squares(): # no empty squares
+            return {'position': None, 'score': 0}
+        
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf} # each score should maximise
+        else: 
+            best = {'position':None, 'score': math.inf} #each score should minimize
+
+        for possible_move in state.available_moves():
+            # step 1: Make a move, try that spot
+            state.make_move(possible_move, player)
+            # step 2: recurse using minimax to simulate a game after making that move
+            sim_score = self.minimax(state, other_player) # now we alternate players
+            # step 3: undo the move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+            # step 4: update the dictionaries if necessary 
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score # replace best
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score # replace best
+
+        return best
